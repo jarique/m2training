f677e18b83c40750d933d9ccc034e399fbcba7c4
Yaroslav Behar <y.behar@dinarys.com>
2020-09-16 12:16:22 +0300

    Task 1.2,1.3,1.4,1.5,1.6 di.xml

---
+++ app/code/Training/TestOM/Model/Test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+class Test
+{
+    private $manager;
+    private $name;
+    private $number;
+    private $arrayList;
+    private $managerFactory;
+
+    public function __construct(
+        \Training\TestOM\Model\ManagerInterface $manager,
+        $name,
+        int $number,
+        array $arrayList,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
+    ) {
+        $this->manager = $manager;
+        $this->name = $name;
+        $this->number = $number;
+        $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
+    }
+
+    public function log()
+    {
+        print_r(get_class($this->manager));
+        echo '<br>';
+        print_r($this->name);
+        echo '<br>';
+        print_r($this->number);
+        echo '<br>';
+        print_r($this->arrayList);
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager));
+    }
+}
