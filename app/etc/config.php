5f5c75d1faf0a8a37009905a47f13f45367be366
Yaroslav Behar <y.behar@dinarys.com>
2020-10-09 16:04:21 +0300

    Task 4.2 add confirmation when adding product to cart

--- app/etc/config.php
+++ app/etc/config.php
@@ -297,7 +297,7 @@ return [
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
         'Training_TestOM' => 1,
-        'Training_Dependency2' => 0,
+        'Training_Dependency2' => 1,
         'Training_Dependency' => 1,
         'Training_Render' => 1,
         'Training_Test' => 1,
